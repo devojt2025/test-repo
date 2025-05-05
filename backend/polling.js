02477444ecb9762406b28568c88a9c4fa68076ac
devojt2025 <dev.ojt@geniex.tech>
2025-05-05 14:21:02 +0800

    updated polling

--- backend/polling.js
+++ backend/polling.js
@@ -4,6 +4,8 @@ const cron = require('node-cron');
 const path = require('path');
 const logFile = 'git-pull-log.txt'
 const repoDirectory = "C:\\Users\\DEV.OJT\\Documents\\test-repo"
+const command = `cd "${repoDirectory}"; git pull; cd frontend; npm install; npm run build; cd ../backend; npm install; pm2 restart app; cd C:/nginx; ./nginx.exe -s reload`;
+
 function ensureDirectoryExists(dirPath) {
     if (!fs.existsSync(dirPath)) {
         console.error(`Directory not found: ${dirPath}`);
@@ -13,12 +15,7 @@ function ensureDirectoryExists(dirPath) {
 }
 function pullLatestCode() {
     console.log('Checking for updates...');
-    exec(`
-        cd ${repoDirectory}; git pull;
-        cd frontend ; npm install ; npm run build ;
-        cd ../backend ; npm install ; pm2 restart app ;
-        cd C:/nginx ; nginx.exe -s reload
-        `, (err, stdout, stderr) => {
+    exec(command, {shell: 'powershell.exe'}, (err, stdout, stderr) => {
         if (err) {
             console.error(`Error pulling code: ${stderr}`);
             fs.appendFileSync(logFile, `ERROR: ${new Date().toISOString()} - ${stderr}\n`)
