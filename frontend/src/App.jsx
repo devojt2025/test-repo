3ea9e6fd9ea1e752c9eae1e64af81f03993eeab1
emmanyouwell <emingala02@gmail.com>
2025-05-07 08:51:36 +0800

    new branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import React,{ useState } from "react";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center">Testing</h1>
+      <h1 className="text-3xl font-bold underline text-center">Development</h1>
     </>
   );
 }
